e06769b684e6d8c10bd6573159d57cd079cf87d9
chaymae lakhal <129671626+chlakhal@users.noreply.github.com>
2026-09-23 19:19:48 +0200

    Solve numpy-create-array

---
+++ study-plans/numpy-basics/numpy-create-array/numpy-create-array.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def create_array(data: list) -> np.ndarray:
+    """
+    Returns a 2D float64 array with the same values and shape as data.
+    """
+    arr=np.array(data,dtype=np.float64)
+    return arr 

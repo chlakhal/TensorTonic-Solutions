2f85ba59ec0c1a2f70a22f419b3f50ac270e3355
chaymae lakhal <129671626+chlakhal@users.noreply.github.com>
2026-09-24 15:42:16 +0200

    Solve pytorch-basic-autograd

---
+++ study-plans/pytorch-basics/pytorch-basic-autograd/pytorch-basic-autograd.py
@@ -0,0 +1,14 @@
+import torch
+
+def compute_gradient(values: torch.Tensor) -> torch.Tensor:
+    """
+    Returns a float32 gradient tensor with the same shape as values.
+    """
+    
+    values.requires_grad_(True)
+
+    y = (values**3 + 2*values).sum()
+
+    y.backward()
+
+    return values.grad
